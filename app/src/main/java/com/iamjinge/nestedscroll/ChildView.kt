3026643c7a8ed63fa3b69517bcaae9606bdcc4b5
eltonjiang <eltonjiang@tencent.com>
2018-05-26 15:38:43 +0800

    scroll in x

--- app/src/main/java/com/iamjinge/nestedscroll/ChildView.kt
+++ app/src/main/java/com/iamjinge/nestedscroll/ChildView.kt
@@ -88,8 +88,8 @@ class ChildView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
                               parentHeightMeasureSpec: Int) {
         val lp = child.layoutParams
 
-        val childWidthMeasureSpec = ViewGroup.getChildMeasureSpec(parentWidthMeasureSpec,
-                paddingLeft + paddingRight, lp.width)
+        val childWidthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED)
 
         val childHeightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0,
                 View.MeasureSpec.UNSPECIFIED)
@@ -101,9 +101,8 @@ class ChildView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
                                          parentHeightMeasureSpec: Int, heightUsed: Int) {
         val lp = child.layoutParams as ViewGroup.MarginLayoutParams
 
-        val childWidthMeasureSpec = ViewGroup.getChildMeasureSpec(parentWidthMeasureSpec,
-                paddingLeft + paddingRight + lp.leftMargin + lp.rightMargin
-                        + widthUsed, lp.width)
+        val childWidthMeasureSpec = View.MeasureSpec.makeMeasureSpec(
+                lp.leftMargin + lp.rightMargin, View.MeasureSpec.UNSPECIFIED)
         val childHeightMeasureSpec = View.MeasureSpec.makeMeasureSpec(
                 lp.topMargin + lp.bottomMargin, View.MeasureSpec.UNSPECIFIED)
 
@@ -114,53 +113,67 @@ class ChildView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
         val vtev = MotionEvent.obtain(event)
         Log.d(TAG, "before offset ${event.y}")
         if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+            nestedOffset[0] = 0
             nestedOffset[1] = 0
         }
-        vtev.offsetLocation(0f, nestedOffset[1].toFloat())
+        vtev.offsetLocation(nestedOffset[0].toFloat(), nestedOffset[1].toFloat())
         when (event.actionMasked) {
             MotionEvent.ACTION_DOWN -> {
                 lastPos[0] = event.x
                 lastPos[1] = event.y
-                startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL)
+                startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL or ViewCompat.SCROLL_AXIS_HORIZONTAL)
                 isDrag = false
             }
 
             MotionEvent.ACTION_MOVE -> {
+                val x = event.x.toInt()
                 val y = event.y.toInt()
-                val ly = lastPos[1]
-                var deltaY: Int = (ly - y).toInt()
-                Log.d(TAG, "last ${ly}, touch $y, move $deltaY")
-//                scrollBy(0, deltaY)
+                var deltaY: Int = (lastPos[1] - y).toInt()
+                var deltaX: Int = (lastPos[0] - x).toInt()
 
                 if (dispatchNestedPreScroll(0, deltaY, scrollConsumed, scrollOffset)) {
 //                    deltaY -= scrollConsumed[1]
 //                    vtev.offsetLocation(0f, scrollOffset[1].toFloat())
 //                    nestedOffset[1] += scrollOffset[1]
                 }
-                if (!isDrag && Math.abs(deltaY) > touchSlop) {
+                if (!isDrag && Math.sqrt((deltaX * deltaX + deltaY * deltaY).toDouble()) > touchSlop) {
                     isDrag = true
-                    if (deltaY > 0) {
-                        deltaY -= touchSlop
-                    } else {
-                        deltaY += touchSlop
-                    }
+//                    if (deltaY > 0) {
+//                        deltaY -= touchSlop
+//                    } else {
+//                        deltaY += touchSlop
+//                    }
                 }
                 if (isDrag) {
+                    lastPos[0] = x.toFloat()
                     lastPos[1] = y.toFloat() //- scrollOffset[1]
+                    val oldX = scrollX
                     val oldY = scrollY
-                    var consumY = deltaY
+                    var consumedX = deltaX
+                    var consumedY = deltaY
+                    if (oldX + deltaX < 0) {
+                        consumedX = 0 - oldX
+                    }
+                    if (oldX + deltaX > getScrollRange()[0]) {
+                        consumedX = getScrollRange()[0] - oldX
+                    }
                     if (oldY + deltaY < 0) {
-                        consumY = 0 - oldY
+                        consumedY = 0 - oldY
                     }
-                    if (oldY + deltaY > getScrollRange()) {
-                        consumY = getScrollRange() - oldY
+                    if (oldY + deltaY > getScrollRange()[1]) {
+                        consumedY = getScrollRange()[1] - oldY
                     }
-                    scrollBy(0, consumY)
-                    val scrolled = scrollY - oldY
-//                    Log.d(TAG, "oldy $oldY, delta $deltaY, height $height ${getChildAt(0).height}, consume $consumY, scroll $scrolled")
-                    if (dispatchNestedScroll(0, scrolled, 0, deltaY - scrolled, scrollOffset)) {
+                    scrollBy(consumedX, consumedY)
+                    val scrolledX = scrollX - oldX
+                    val scrolledY = scrollY - oldY
+
+                    if (dispatchNestedScroll(scrolledX, scrolledY,
+                                    deltaX - scrolledX, deltaY - scrolledY,
+                                    scrollOffset)) {
+                        lastPos[0] -= scrollOffset[0].toFloat()
                         lastPos[1] -= scrollOffset[1].toFloat()
-                        vtev.offsetLocation(0f, scrollOffset[1].toFloat())
+                        vtev.offsetLocation(scrollOffset[0].toFloat(), scrollOffset[1].toFloat())
+                        nestedOffset[0] += scrollOffset[0]
                         nestedOffset[1] += scrollOffset[1]
                     }
 //                    scrollBy(0, scrollOffset[1])
@@ -186,11 +199,12 @@ class ChildView(context: Context, attrs: AttributeSet?, defStyleAttr: Int)
 
     }
 
-    fun getScrollRange(): Int {
-        var scrollRange = 0;
+    fun getScrollRange(): IntArray {
+        var scrollRange = IntArray(2)
         if (childCount > 0) {
             var child = getChildAt(0)
-            scrollRange = Math.max(0, child.height - (height - paddingBottom - paddingTop))
+            scrollRange[0] = Math.max(0, child.width - (width - paddingBottom - paddingTop))
+            scrollRange[1] = Math.max(0, child.height - (height - paddingBottom - paddingTop))
         }
         return scrollRange
     }
